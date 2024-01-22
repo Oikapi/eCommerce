d2f3ca9766e9ab4d6160ce78807cd7e85183bd80
Oikapi <seryy.gordeev.78@mail.ru>
2024-01-23 02:06:44 +0700

    migrate-from-cra-to-vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import Header from  './components/header/Header'
+import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
 import React from "react"
 import MainSection from './components/mainSection/MainSection';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import EachPage from "./components/eachPage/EachPage.jsx"
-import {DataProvider} from './components/dataProvider';
+import { DataProvider } from './components/dataProvider';
 import Section from './components/Section';
 
 class App extends React.Component {
   render() {
     return (
       <DataProvider>
-          <div className="App">
+        <div className="App">
           <Router>
-            <Header/>
-            <Section/>
-            <Footer/>
+            <Header />
+            <Section />
+            <Footer />
           </Router>
-          </div>
+        </div>
       </DataProvider>
     );
   }
